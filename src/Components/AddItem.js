3dce7082cf8bff658bd6610588ecdbba896e2ce6
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-24 15:18:32 -0400

    Added functional delete component and started on date and time component

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import DateAndTimePickers from './DateTime';
 
+import DateTime from './DateTime';
 const AddItem = () => {
     // Sets up state and baseURL to be used
     const [item, setItem] = useState(null);
-    
+
+    const [todo, setTodo] = useState({
+        name: '',
+        dueDate: '2021-01-25T12:00Z',
+        isComplete: false
+    });
+
     const baseURL = "https://localhost:5001/todos";
   
 
@@ -26,20 +34,40 @@ const AddItem = () => {
         });
     }
 
+    const handleNameInputChange = (event) => {
+        event.persist();
+        setTodo((todo) => ({
+            ...todo,
+            name: event.target.value,
+        }));
+    }
+    const handleDueDateInputChange = (event) => {
+        event.persist();
+        setTodo((todo) => ({
+            ...todo,
+            dueDate: event.target.value,
+        }));
+    }
+    
+
+
     if(!item) return "No item";
 
     return (
         <div>
             <form>
                 <label>
-                    Name:
-                    <input type="text" value="name" />
+                    Title:
+                    <input onChange={handleNameInputChange} placeholder="Name" type="text" value={todo.name} />
                 </label>
                 <input type="submit" value="submit" />
             </form>
+            <hr />
+            <DateAndTimePickers date={todo.dueDate}/>
             <h1>{item.name}</h1>
             <p>{item.dueDate}</p>
-            <button onClick={createItem}>Create Item</button>
+        <button onClick={createItem}>Create Item</button>
+            
         </div>
     );
 }
--- src/Components/Delete.js
+++ src/Components/Delete.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
 
 const DeleteItem = () => {
-    return <div> Delete Item</div>
+    let { id } = useParams();
+    const [todo, setTodo] = useState(null);
+    const baseURL = `https://localhost:5001/todos/${id}/delete`;
+
+    useEffect(() => {
+        axios.get(`https://localhost:5001/todos/${id}`).then((response) => {
+            setTodo(response.data);
+        })
+    }, []);
+
+    const deleteTodo = () => {
+        axios
+            .delete(`https://localhost:5001/todos/${id}`)
+            .then(() => {
+                alert("Post deleted");
+                setTodo(null)
+            });
+    }
+
+    if (!todo) return "No post!"
+
+
+    return (
+    <div>
+        <h1>{todo.name}</h1>
+        <p>{todo.dueDate}</p>
+        <button onClick={deleteTodo}>Delete Todo</button>
+    </div>
+    )
 }
 
 
